1bdea50574c2f1898e637703dd79f08ae4b9c000
jsan <ahsan.wtc@gmail.com>
2022-04-20 13:44:15 +0500

    adding deposit functions

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -7,7 +7,17 @@ import './Compound.sol';
 contract Wallet is Compound {
   address public admin;
 
-  constructor(address _comptroller, address _cEthAddress) Compound(_comptroller, _cEthAddress) public {
+  constructor(address _comptroller, address _cEthAddress) Compound(_comptroller, _cEthAddress) {
     admin = msg.sender;
+  }
+
+  function deposit(address _cTokenAddress, uint _underlyingAmount) external {
+    address underlyingTokenAddress = getUnderlyingAddress(_cTokenAddress);
+    IERC20(underlyingTokenAddress).transferFrom(msg.sender, address(this), _underlyingAmount);
+    supply(_cTokenAddress, _underlyingAmount);
+  }
+
+  receive() payable external {
+    supplyEth(msg.value);
   }
 }
